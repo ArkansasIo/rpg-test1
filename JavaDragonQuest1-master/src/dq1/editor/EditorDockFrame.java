aa8554021178b7d603d02de88509909df209d921
stephen <95516724+ArkansasIo@users.noreply.github.com>
2026-02-22 03:32:51 -0500

    Editor: wire map cell selection to inspector; add EditorAudioAPI; SFX parameter controls & preset save

--- JavaDragonQuest1-master/src/dq1/editor/EditorDockFrame.java
+++ JavaDragonQuest1-master/src/dq1/editor/EditorDockFrame.java
@@ -1,6 +1,8 @@
 package dq1.editor;
 
 import dq1.editor.audio.AudioToolsPanel;
+import dq1.editor.audio.EditorAudioAPI;
+import dq1.editor.audio.AudioPlaybackUtil;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
@@ -11,6 +13,7 @@ public class EditorDockFrame extends JFrame {
 
     private AssetBrowserPanel browser;
     private InspectorPanel inspector;
+    private MapEditorCanvasPanel mapCanvas;
 
     public EditorDockFrame() {
         super("Editor Dock");
@@ -19,18 +22,37 @@ public class EditorDockFrame extends JFrame {
 
         browser = new AssetBrowserPanel();
         inspector = new InspectorPanel();
+        mapCanvas = new MapEditorCanvasPanel();
 
-        JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, browser, inspector);
-        split.setDividerLocation(300);
-        add(split, BorderLayout.CENTER);
+        JSplitPane rightSplit = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, mapCanvas, inspector);
+        rightSplit.setDividerLocation(520);
+
+        JSplitPane mainSplit = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, browser, rightSplit);
+        mainSplit.setDividerLocation(300);
+        add(mainSplit, BorderLayout.CENTER);
 
         // Add a small tools panel (audio tools) docked at bottom
         add(new AudioToolsPanel(), BorderLayout.SOUTH);
 
         browser.addSelectionListener(e -> onBrowserSelection(e));
 
+        // wire map cell selection to inspector to show audio attachment
+        mapCanvas.setCellSelectionListener((row, col) -> {
+            String attached = mapCanvas.getAudioAttachment(row, col);
+            if (attached != null) {
+                File f = EditorAudioAPI.getAudioFile(attached);
+                if (f != null) {
+                    inspector.inspect(f);
+                } else {
+                    inspector.inspect(null);
+                }
+            } else {
+                inspector.inspect(null);
+            }
+        });
+
         pack();
-        setSize(900, 700);
+        setSize(1200, 800);
         setLocationRelativeTo(null);
     }
 
