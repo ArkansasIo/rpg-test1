dd766a5df3597a75eb68a08437eb34a1256ee1a3
stephen <95516724+ArkansasIo@users.noreply.github.com>
2026-02-22 03:52:14 -0500

    Credits: replace original staff list with 'By: Stephen Deline Jr' per request

--- JavaDragonQuest1-master/src/dq1/core/Credits.java
+++ JavaDragonQuest1-master/src/dq1/core/Credits.java
@@ -11,139 +11,11 @@ import javax.imageio.ImageIO;
  */
 public class Credits {
 
-    static String msg = "\n\n" +
-"Original Credits\n" +
-"\n" +
-"\n" +
-"\n" +
-"Original Staff:\n" +
-"\n" +
-"\n" +
-"\n" +
-"Scenario Written by:     \n" +
-"\n" +           
-"   Yuji Horii\n" +
-"\n" +           
-"\n" +           
-"\n" +           
-"Character Designed by:   \n" + 
-"\n" +
-"   Akira Toriyama\n" +
-"\n" +           
-"\n" +           
-"\n" +           
-"Music Composed by:       \n" + 
-"\n" +
-"   Koichi Sugiyama\n" +
-"\n" +           
-"\n" +           
-"\n" +           
-"Programmed by:           \n" + 
-"\n" +
-"   Koichi Nakamura\n" +
-"   Koji Yoshida\n" +
-"   Takenori Yamamori\n" +
-"\n" +           
-"\n" +           
-"\n" +           
-"CG Designed by:          \n" + 
-"\n" +
-"   Takashi Yasuno\n" +
-"\n" +           
-"\n" +           
-"\n" +           
-"Scenario Assisted by:    \n" + 
-"\n" +
-"   Hiroshi Miyaoka\n" +
-"\n" +           
-"\n" +           
-"\n" +           
-"Assisted by:             \n" +
-"\n" +
-"   Rika Suzuki\n" +
-"   Tadashi Fukuzawa\n" +
-"\n" +           
-"\n" +           
-"\n" +           
-"Special Thanks to:       \n" + 
-"\n" +
-" Kazuhiko Torishima\n" +
-"\n" +
-"\n" +
-"\n" +
-"Translation Staff:\n" +
-"\n" +
-"\n" +
-"\n" +
-"Translated by:           \n" + 
-"\n" +
-"   Toshiko Watson\n" +
-"\n" +
-"\n" +
-"\n" +
-"Revised Text by:         \n" + 
-"\n" +
-"   Scott Pelland\n" +
-"\n" +
-"\n" +
-"\n" +
-"Technical Support by:    \n" + 
-"\n" +
-"   Doug Baker\n" +
-"\n" +
-"\n" +
-"\n" +
-"Programmed by:           \n" + 
-"\n" +
-"   Kenichi Masuta\n" +
-"   Manabu Yamana\n" +
-"\n" +
-"\n" +
-"\n" +
-"CG Designed by:          \n" + 
-"\n" +
-"   Satoshi Fudaba\n" +
-"\n" +
-"\n" +
-"\n" +
-"Special Thanks to:       \n" +
-"\n" +
-"   Howard Phillips\n" +
-"\n" +
-"\n" +
-"\n" +
-"Directed by:             \n" +
-"\n" +
-"   Koichi Nakamura\n" +
-"\n" +
-"\n" +
-"\n" +
-"Produced by:             \n" +
-"\n" +
-"   Yukinobu Chida\n" +
-"\n" +
-"\n" +
-"\n" +
-"Java PC version by:       \n" +
-"\n" +
-"   \n" +
-"\n" +
-"\n" +
-"\n" +
-"Based on DRAGON QUEST\n" +
-"\n" +
-"\n" +
-"\n" +
-"Copyright\n" +
-"\n" +
-"\n" +
-"\n" +
-"\n" +
-"\n";
-    
+    static String msg = "\n\nBy: Stephen Deline Jr\n";
+
     public static void main(String[] args) throws Exception {
         BitmapFont font = new BitmapFont();
-        BufferedImage result = new BufferedImage(256, 240 * 10, BufferedImage.TYPE_INT_RGB);
+        BufferedImage result = new BufferedImage(256, 240, BufferedImage.TYPE_INT_RGB);
         Graphics2D g = (Graphics2D) result.getGraphics();
         String[] lines = msg.split("\n");
         int lineNumber = 0;
@@ -153,5 +25,5 @@ public class Credits {
         }
         ImageIO.write(result, "png", new File("d:/dq1_credits.png"));
     }
-    
+
 }
